736874eb1f1ecb97f0a79b28820ac259affad65c
Prelang Builder <builder@prelang.com>
2014-07-04 00:25:14 +0000

    Generated model Page with migration

---
+++ spec/factories/pages.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :page do
+    title "MyString"
+    synopsis "MyText"
+    content "MyText"
+  end
+end
